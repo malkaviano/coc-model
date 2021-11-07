5d0f1d1a7fd68fefa21c0688f1d5c15f9b4324b0
Malkaviano <rafael.kss@gmail.com>
2021-11-07 20:15:32 -0300

    [FEATURE] Add luck char

---
+++ src/main/scala/com/malk/coc/v7/concepts/characteristics/Luck.scala
@@ -0,0 +1,3 @@
+package com.malk.coc.v7.concepts.characteristics
+
+case class Luck(override val value: Int) extends Characteristic("Luck", value)
---
+++ src/test/scala/com/malk/coc/v7/concepts/characteristics/LuckSpec.scala
@@ -0,0 +1,18 @@
+package com.malk.coc.v7.concepts.characteristics
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class LuckSpec extends AnyFunSpec with Matchers {
+  describe("The Luck") {
+    val luck = Luck(40)
+
+    it("should have name Luck") {
+      luck.name shouldBe "Luck"
+    }
+
+    it("should have value 40") {
+      luck.value shouldBe 40
+    }
+  }
+}
