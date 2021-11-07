3383eb3ce0bc098844c733a5933eb70aff7e083b
Malkaviano <rafael.kss@gmail.com>
2021-11-07 20:23:47 -0300

    [FEATURE] Add age char

---
+++ src/main/scala/com/malk/coc/v7/concepts/characteristics/Age.scala
@@ -0,0 +1,3 @@
+package com.malk.coc.v7.concepts.characteristics
+
+case class Age(override val value: Int) extends Characteristic("Age", value)
---
+++ src/test/scala/com/malk/coc/v7/concepts/characteristics/AgeSpec.scala
@@ -0,0 +1,18 @@
+package com.malk.coc.v7.concepts.characteristics
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class AgeSpec extends AnyFunSpec with Matchers {
+  describe("The Age") {
+    val age = Age(21)
+
+    it("should have name Age") {
+      age.name shouldBe "Age"
+    }
+
+    it("should have value 21") {
+      age.value shouldBe 21
+    }
+  }
+}
