77efd2ac74b1b0d7ebd4853cd52b13940a689919
Malkaviano <rafael.kss@gmail.com>
2021-11-08 11:21:42 -0300

    [REFACTOR] Rename Education trait to Knowledge

--- src/main/scala/com/malk/coc/traits/Knowledge.scala
+++ src/main/scala/com/malk/coc/traits/Knowledge.scala
@@ -1,5 +1,5 @@
 package com.malk.coc.traits
 
-trait Education {
+trait Knowledge {
   def EDU: Int
 }
--- src/test/scala/com/malk/coc/traits/KnowledgeSpec.scala
+++ src/test/scala/com/malk/coc/traits/KnowledgeSpec.scala
@@ -3,8 +3,8 @@ package com.malk.coc.traits
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
-class EducationSpec extends AnyFunSpec with Matchers {
-  val edu = new Education {
+class KnowledgeSpec extends AnyFunSpec with Matchers {
+  val edu = new Knowledge {
     override def EDU: Int = 76
   }
 
