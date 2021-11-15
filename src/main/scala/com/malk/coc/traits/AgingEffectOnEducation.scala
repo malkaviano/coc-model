5f215b82cb4fc060cd94ca3ed9b55c34078a75d6
Malkaviano <rafael.kss@gmail.com>
2021-11-15 13:02:27 -0300

    [REFACTOR] Remove random Aging rules

--- src/main/scala/com/malk/coc/traits/AgingEffectOnEducation.scala
+++
@@ -1,8 +0,0 @@
-package com.malk.coc.traits
-
-import com.malk.coc.concepts.characteristics.Age
-import com.malk.coc.concepts.characteristics.Education
-
-trait AgingEffectOnEducation {
-  def modifiedEducation(age: Age, edu: Education): Education
-}
