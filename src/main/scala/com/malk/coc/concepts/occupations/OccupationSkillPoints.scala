1942fe098ca0261e95689d16b98f3fdc8fbf1b83
Malkaviano <rafael.kss@gmail.com>
2021-11-18 08:20:08 -0300

    [FEATURE] Add OccupationSkillPoints spend

--- src/main/scala/com/malk/coc/concepts/occupations/OccupationSkillPoints.scala
+++ src/main/scala/com/malk/coc/concepts/occupations/OccupationSkillPoints.scala
@@ -1,3 +1,14 @@
 package com.malk.coc.concepts.occupations
 
-final case class OccupationSkillPoints(private val total: Int)
+final case class OccupationSkillPoints(private var total: Int) {
+  def spend(points: Int): Int = {
+    val spent = points match {
+      case x if x <= total => x
+      case _ => total
+    }
+
+    total -= spent
+
+    spent
+  }
+}
