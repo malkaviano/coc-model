af8318d0d46c5e6f77e7d1dd4f3c8f49f5481cb1
Malkaviano <rafael.kss@gmail.com>
2021-11-17 23:13:09 -0300

    [FIX] Change Throw to NotPushable

--- src/main/scala/com/malk/coc/concepts/skills/Throw.scala
+++ src/main/scala/com/malk/coc/concepts/skills/Throw.scala
@@ -1,9 +1,9 @@
 package com.malk.coc.concepts.skills
 
 import com.malk.coc.traits.Skill
-import com.malk.coc.traits.PushableSkill
+import com.malk.coc.traits.NotPushableSkill
 
-final case class Throw(spent: Int) extends Skill(spent) with PushableSkill {
+final case class Throw(spent: Int) extends Skill(spent) with NotPushableSkill {
   override def base: Int = 20
 
   override def name: String = "Throw"
