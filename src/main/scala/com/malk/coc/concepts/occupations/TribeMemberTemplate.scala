d7cb357beb1cce0c30493136b10007d7f11188db
Malkaviano <rafael.kss@gmail.com>
2021-11-20 09:17:27 -0300

    [REFACTOR] Change skill decisions to inside template

--- src/main/scala/com/malk/coc/concepts/occupations/TribeMemberTemplate.scala
+++ src/main/scala/com/malk/coc/concepts/occupations/TribeMemberTemplate.scala
@@ -5,13 +5,18 @@ import com.malk.coc.concepts.skills._
 import com.malk.coc.helpers.SkillHelper
 import com.malk.coc.rules.TwoEduEitherTwoStrOrDexRule
 import com.malk.coc.traits.OccupationTemplate
+import com.malk.coc.concepts.abstractions.Body
+import com.malk.coc.concepts.abstractions.Brain
+import com.malk.coc.concepts.characteristics.Education
+import com.malk.coc.concepts.characteristics.Appearance
+import com.malk.coc.concepts.characteristics.Dexterity
 
 final class TribeMemberTemplate extends OccupationTemplate {
   val name = TribeMemberTemplate.name
 
-  val startCreditRating = CreditRating()
+  def startCreditRating = CreditRating()
 
-  val maximumCreditRating = {
+  def maximumCreditRating = {
     val cr = CreditRating()
 
     cr.spend(15)
@@ -41,7 +46,37 @@ final class TribeMemberTemplate extends OccupationTemplate {
     (1, SkillHelper.fightingSkills ++ Set(Throw()))
   )
 
+  def nonTrainableSkills: Set[Skill] = Set(CthulhuMythos())
+
+  def excludedSkills: Set[Skill] =
+    SkillHelper.modernSkills ++ SkillHelper.uncommonSkills
+
+  def personalSkills: Set[Skill] = SkillHelper.filteredSkills(
+    nonTrainableSkills ++ excludedSkills + Dodge(
+      Dexterity(0)
+    )() + LanguageOwn(Education(0))() + CreditRating()
+  )
+
   val occupationSkillPointsRule = new TwoEduEitherTwoStrOrDexRule
+
+  def templateSkills(
+      body: Body,
+      brain: Brain,
+      edu: Education,
+      app: Appearance
+  ): (Set[Skill], Set[(Int, Set[Skill])], Set[Skill], Set[Skill]) = {
+    val selfSkills = Set(
+      Dodge(body.dexterity)(),
+      LanguageOwn(edu)()
+    )
+
+    (
+      fixedSkills,
+      optionalSkills,
+      personalSkills ++ selfSkills,
+      nonTrainableSkills
+    )
+  }
 }
 
 object TribeMemberTemplate {
--- src/test/scala/com/malk/coc/concepts/occupations/TribeMemberTemplateSpec.scala
+++ src/test/scala/com/malk/coc/concepts/occupations/TribeMemberTemplateSpec.scala
@@ -5,10 +5,16 @@ import org.scalatest.matchers.should.Matchers
 import com.malk.coc.concepts.skills._
 import com.malk.coc.traits.Skill
 import com.malk.coc.rules.TwoEduEitherTwoStrOrDexRule
+import com.malk.coc.helpers.SkillHelper
+import com.malk.coc.concepts.characteristics.Dexterity
+import com.malk.coc.concepts.characteristics.Education
 
 class TribeMemberTemplateSpec extends AnyFunSpec with Matchers {
+  import com.malk.coc.helpers.InvestigatorCharacteristics.implicits._
+  import com.malk.coc.helpers.DiceHelper.implicits._
+
   describe("TRIBE MEMBER occupation") {
-    val occupation = new TribeMemberTemplate
+    val occupationTemplate = new TribeMemberTemplate
     val startCreditRating = CreditRating()
     val maximumCreditRating = CreditRating()
 
@@ -41,32 +47,69 @@ class TribeMemberTemplateSpec extends AnyFunSpec with Matchers {
       )
     )
 
+    val nonTrainableSkills = Set(CthulhuMythos())
+
+    val excludedSkills = SkillHelper.uncommonSkills ++ SkillHelper.modernSkills
+
+    val implicitBody = body
+    val implicitBrain = brain
+    val implicitEdu = edu
+    val implicitApp = app
+
+    val selfSkills = Set(
+      Dodge(implicitBody.dexterity)(),
+      LanguageOwn(implicitEdu)()
+    )
+
+    val personalSkills: Set[Skill] = SkillHelper.filteredSkills(
+      Set(CreditRating(), CthulhuMythos()) ++ excludedSkills + Dodge(
+        Dexterity(0)
+      )() + LanguageOwn(Education(0))()
+    ) ++ selfSkills
+
     val occupationSkillPointsRule = new TwoEduEitherTwoStrOrDexRule
 
     it("should have name TRIBE MEMBER") {
-      occupation.name shouldBe "TRIBE MEMBER"
+      occupationTemplate.name shouldBe "TRIBE MEMBER"
     }
 
-    it(s"should have start ${startCreditRating}") {
-      occupation.startCreditRating shouldBe startCreditRating
-    }
+    describe("getting template skills") {
+      it(s"should have start ${startCreditRating}") {
+        occupationTemplate.startCreditRating shouldBe startCreditRating
+      }
 
-    it(s"should have maximum ${maximumCreditRating}") {
-      occupation.maximumCreditRating shouldBe maximumCreditRating
-    }
+      it(s"should have maximum ${maximumCreditRating}") {
+        occupationTemplate.maximumCreditRating shouldBe maximumCreditRating
+      }
 
-    it(s"should have a list of fixed skills") {
-      occupation.fixedSkills should contain theSameElementsAs fixedSkills
-    }
+      val result = occupationTemplate.templateSkills(
+        implicitBody,
+        implicitBrain,
+        implicitEdu,
+        implicitApp
+      )
 
-    it(s"should have a list of optional skills") {
-      occupation.optionalSkills should contain theSameElementsAs optionalSkills
+      it(s"should have a list of fixed skills") {
+        result._1 should contain theSameElementsAs fixedSkills
+      }
+
+      it(s"should have a list of optional skills") {
+        result._2 should contain theSameElementsAs optionalSkills
+      }
+
+      it(s"should have a list of personal skills") {
+        result._3 should contain theSameElementsAs personalSkills
+      }
+
+      it(s"should have a list of non trainable skills") {
+        result._4 should contain theSameElementsAs nonTrainableSkills
+      }
     }
 
     it(
       "should have Occupation Skill Points Rule equal TwoEduEitherTwoStrOrDexRule"
     ) {
-      occupation.occupationSkillPointsRule.name shouldBe occupationSkillPointsRule.name
+      occupationTemplate.occupationSkillPointsRule.name shouldBe occupationSkillPointsRule.name
     }
   }
 }
