19f7d4603a528d950f46054b29b6841bb7733dc7
Malkaviano <rafael.kss@gmail.com>
2021-11-21 14:06:11 -0300

    [FEATURE] CreditRating business logic

--- src/main/scala/com/malk/coc/concepts/skills/CreditRating.scala
+++ src/main/scala/com/malk/coc/concepts/skills/CreditRating.scala
@@ -4,6 +4,19 @@ import com.malk.coc.traits.Skill
 import com.malk.coc.traits.PushableSkill
 import com.malk.coc.traits.WithoutBaseValueSkill
 
-final case class CreditRating() extends Skill with PushableSkill with WithoutBaseValueSkill {
+final case class CreditRating(val initial: Int = 0, val maximum: Int = 30)
+    extends Skill
+    with PushableSkill
+    with WithoutBaseValueSkill {
+  private var internalValue: Int = initial
+
   override def name: String = "Credit Rating"
+
+  override def spend(points: Int): Unit = {
+    if (internalValue + points <= maximum) {
+      internalValue += points
+    }
+  }
+
+  override def value: Int = internalValue
 }
--- src/test/scala/com/malk/coc/concepts/skills/CreditRatingSpec.scala
+++ src/test/scala/com/malk/coc/concepts/skills/CreditRatingSpec.scala
@@ -3,9 +3,17 @@ package com.malk.coc.concepts.skills
 class CreditRatingSpec extends BehavesLikeSkill with BehavesLikeSkillComparing {
   val skillName = "Credit Rating"
 
-  val skill = CreditRating()
+  val skill = CreditRating(0, 30)
 
   behavesLikeSkill(skill, skillName, 0, true, 30)
 
   behavesLikeSkillComparing(skill, skillName, 0, true)
+
+  describe("Spending over the maximum") {
+    it("should not allow going over maximum") {
+      skill.spend(10)
+
+      skill.value shouldBe 30
+    }
+  }
 }
