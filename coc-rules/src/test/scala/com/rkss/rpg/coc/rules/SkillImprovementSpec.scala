fe2a42f3bcb2993e0ff48ddd1a55dceba166d85b
Malkaviano <rafael.kss@gmail.com>
2021-12-25 13:07:13 -0300

    [SPEC] Clear spec

--- coc-rules/src/test/scala/com/rkss/rpg/coc/rules/SkillImprovementSpec.scala
+++ coc-rules/src/test/scala/com/rkss/rpg/coc/rules/SkillImprovementSpec.scala
@@ -6,17 +6,15 @@ import org.scalatest.matchers.should.Matchers
 import com.rkss.rpg.coc.props.TestingProps
 
 import com.rkss.rpg.helpers.dice._
-import com.rkss.rpg.coc.props.fakes.FakeSkill
-import com.rkss.rpg.coc.concepts.skill._
 import com.rkss.rpg.helpers.traits.DiceResult
-import com.rkss.rpg.coc.props.fakes.FakeDiceResult
+import com.rkss.rpg.coc.props.fakes._
 
 class SkillImprovementSpec extends AnyFunSpec with Matchers {
   describe("Improving a skill") {
     describe("Improvement check") {
       describe("when improvement check fails") {
         it("should have same improve value") {
-          val result = skillImprovement(Seq(1), Seq(10), true)
+          val result = skillImprovement(Seq(1), Seq(10))
 
           result shouldBe Option.empty[DiceResult]
         }
@@ -24,7 +22,7 @@ class SkillImprovementSpec extends AnyFunSpec with Matchers {
 
       describe("when improvement check is equal or superior to 95") {
         it("should have higher improve value") {
-          val result = skillImprovement(Seq(95), Seq(8), true)
+          val result = skillImprovement(Seq(95), Seq(8))
 
           result shouldBe Some(FakeDiceResult(8))
         }
@@ -32,44 +30,17 @@ class SkillImprovementSpec extends AnyFunSpec with Matchers {
 
       describe("when improvement check is superior to skill value") {
         it("should have higher improve value") {
-          val result = skillImprovement(Seq(80), Seq(6), true)
+          val result = skillImprovement(Seq(80), Seq(6))
 
           result shouldBe Some(FakeDiceResult(6))
         }
       }
-
-      describe("when skill used check is false") {
-        it("should have same improve value") {
-          val result = skillImprovement(Seq(96), Seq(8), false)
-
-          result shouldBe Option.empty[DiceResult]
-        }
-      }
     }
-
-    // describe("verify if the skill was used with success") {
-    //   describe("when skill was never used") {
-    //     it(s"returns false") {
-    //       val skill = ImprovableFakeSkill("SomeSkill", 10, 10, 5, 2)
-
-    //       skill.usedWithSuccess shouldBe false
-    //     }
-    //   }
-
-    //   describe("when skill was ticked") {
-    //     val skill = ImprovableFakeSkill("SomeSkill", 10, 10, 5, 2)
-
-    //     skill.tickUsedWithSuccess()
-
-    //     skill.usedWithSuccess shouldBe true
-    //   }
-    // }
   }
 
   private def skillImprovement(
       rolledTest: Seq[Int],
-      rolledImprovement: Seq[Int],
-      skillUseSucceeded: Boolean
+      rolledImprovement: Seq[Int]
   ): Option[DiceResult] = {
     val hundredSidedDice = HundredSidedDice(
       TestingProps.fakeRng(rolledTest)
@@ -79,25 +50,18 @@ class SkillImprovementSpec extends AnyFunSpec with Matchers {
       TestingProps.fakeRng(rolledImprovement)
     )
 
-    val improvableSkill = ImprovableFakeSkill("SomeSkill", 10, 10, 5, 2, skillUseSucceeded)
-
-    improvableSkill.improvementCheck(hundredSidedDice, tenSidedDice)
+    FakeSkillImprovement("fake", 50, 50, 25, 10).improvementCheck(
+      hundredSidedDice,
+      tenSidedDice
+    )
   }
 }
 
-final case class ImprovableFakeSkill(
+final case class FakeSkillImprovement(
     override val name: String,
     override val baseValue: Int,
     override val regular: Int,
     override val hard: Int,
-    override val extreme: Int,
-    override val usedWithSuccess: Boolean
+    override val extreme: Int
 ) extends FakeSkill(name, baseValue, regular, hard, extreme)
-    with SkillImprovable
-    with SkillImprovement {
-
-  override def tickSuccessfullyUsed(): Unit = ???
-
-  override def improvedValue: Int = ???
-
-}
+    with SkillImprovement
--- coc-rules/src/test/scala/com/rkss/rpg/coc/rules/behaviors/ImprovableSkillBehaviorSpec.scala
+++ coc-rules/src/test/scala/com/rkss/rpg/coc/rules/behaviors/ImprovableSkillBehaviorSpec.scala
@@ -4,5 +4,9 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.funspec.AnyFunSpec
 
 class ImprovableSkillBehaviorSpec extends AnyFunSpec with Matchers {
+  describe("Improvable skill behavior") {
+    describe("Check if the skill used with success") {
 
+    }
+  }
 }
