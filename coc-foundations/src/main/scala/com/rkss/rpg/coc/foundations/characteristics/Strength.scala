f466e6e4133c1065b88ef8dafbdc0d7f773f67a1
Malkaviano <rafael.kss@gmail.com>
2021-12-24 22:57:23 -0300

    [REFACTOR] Strength name

--- coc-foundations/src/main/scala/com/rkss/rpg/coc/foundations/characteristics/Strength.scala
+++ coc-foundations/src/main/scala/com/rkss/rpg/coc/foundations/characteristics/Strength.scala
@@ -3,5 +3,8 @@ package com.rkss.rpg.coc.foundations.characteristics
 import com.rkss.rpg.coc.concepts.PrimaryCharacteristic
 import com.rkss.rpg.coc.rules.behaviors.BaseSkillBehavior
 
-final case class Strength(val name: String, override val baseValue: Int)
-    extends PrimaryCharacteristic with BaseSkillBehavior
+final case class Strength(override val baseValue: Int)
+    extends PrimaryCharacteristic
+    with BaseSkillBehavior {
+  override val name: String = "Strength"
+}
