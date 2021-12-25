ad4f3b887cb4ed01836f6ea436bcfdc313b131ca
Malkaviano <rafael.kss@gmail.com>
2021-12-25 13:25:54 -0300

    [SPEC] Mark spec as pending

--- coc-rules/src/test/scala/com/rkss/rpg/coc/rules/behaviors/ImprovableSkillBehaviorSpec.scala
+++ coc-rules/src/test/scala/com/rkss/rpg/coc/rules/behaviors/ImprovableSkillBehaviorSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.funspec.AnyFunSpec
 class ImprovableSkillBehaviorSpec extends AnyFunSpec with Matchers {
   describe("Improvable skill behavior") {
     describe("Check if the skill used with success") {
-
+      pending
     }
   }
 }
