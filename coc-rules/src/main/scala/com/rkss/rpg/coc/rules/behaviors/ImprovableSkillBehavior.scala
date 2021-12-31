8db90898e9c9c5b383333467027a4606516b1d91
Malkaviano <rafael.kss@gmail.com>
2021-12-31 00:10:58 -0300

    [REFACTOR] Remove wrong override

--- coc-rules/src/main/scala/com/rkss/rpg/coc/rules/behaviors/ImprovableSkillBehavior.scala
+++ coc-rules/src/main/scala/com/rkss/rpg/coc/rules/behaviors/ImprovableSkillBehavior.scala
@@ -1,7 +1,6 @@
 package com.rkss.rpg.coc.rules.behaviors
 
 import com.rkss.rpg.coc.concepts.skill._
-import com.rkss.rpg.coc.concepts.skill.roll._
 import com.rkss.rpg.coc.rules._
 
 trait ImprovableSkillBehavior {
@@ -14,12 +13,6 @@ trait ImprovableSkillBehavior {
   private var _usedWithSuccess: Boolean = false
 
   override def usedWithSuccess: Boolean = _usedWithSuccess
-
-  override def value(
-      difficulty: SkillRollDifficultyLevel = RegularDifficulty
-  ): Int = {
-    SkillRollValue(baseValue + improvedValue).value(difficulty)
-  }
 
   override def tickSuccessfullyUsed(): Unit = _usedWithSuccess = true
 
