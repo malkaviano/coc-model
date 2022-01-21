a7356b30474a4d76b9585b7f84dcc88c23b1fa87
Malkaviano <rafael.kss@gmail.com>
2022-01-20 21:05:50 -0300

    [FEATURE] Add SkillInfo and tags

--- coc-foundations/src/main/scala/com/rkss/rpg/coc/foundations/skills/SkillFactory.scala
+++ coc-foundations/src/main/scala/com/rkss/rpg/coc/foundations/skills/SkillFactory.scala
@@ -3,97 +3,97 @@ package com.rkss.rpg.coc.foundations.skills
 import com.rkss.rpg.coc.concepts.skill._
 
 object SkillFactory {
-  val basicSkills: Map[SimpleSkillName, Int] = Map(
-    Accounting -> 5,
-    AnimalHandling -> 5,
-    Anthropology -> 1,
-    Appraise -> 5,
-    Archaeology -> 1,
-    Artillery -> 1,
-    Charm -> 15,
-    Climb -> 20,
-    ComputerUse -> 5,
-    Demolitions -> 1,
-    Disguise -> 5,
-    Diving -> 1,
-    DriveAuto -> 20,
-    ElectricalRepair -> 10,
-    Electronics -> 1,
-    FastTalk -> 5,
-    FirstAid -> 30,
-    History -> 5,
-    Hypnosis -> 1,
-    Intimidate -> 15,
-    Jump -> 20,
-    Law -> 5,
-    LibraryUse -> 20,
-    Listen -> 20,
-    Locksmith -> 1,
-    MechanicalRepair -> 10,
-    Medicine -> 1,
-    NaturalWorld -> 10,
-    Navigate -> 10,
-    Occult -> 5,
-    OperateHeavyMachinery -> 1,
-    Persuade -> 10,
-    Psychoanalysis -> 1,
-    Psychology -> 10,
-    ReadLips -> 1,
-    Ride -> 5,
-    SleightOfHand -> 10,
-    SpotHidden -> 25,
-    Stealth -> 20,
-    Swim -> 20,
-    Track -> 10,
+  val basicSkills: Map[SimpleSkillName, SkillInfo] = Map(
+    Accounting -> SkillInfo(5),
+    AnimalHandling -> SkillInfo(5, Seq(UncommonSkill)),
+    Anthropology -> SkillInfo(1),
+    Appraise -> SkillInfo(5),
+    Archaeology -> SkillInfo(1),
+    Artillery -> SkillInfo(1, Seq(UncommonSkill)),
+    Charm -> SkillInfo(15),
+    Climb -> SkillInfo(20),
+    ComputerUse -> SkillInfo(5, Seq(ModernSkill)),
+    Demolitions -> SkillInfo(1, Seq(UncommonSkill)),
+    Disguise -> SkillInfo(5),
+    Diving -> SkillInfo(1, Seq(UncommonSkill)),
+    DriveAuto -> SkillInfo(20),
+    ElectricalRepair -> SkillInfo(10),
+    Electronics -> SkillInfo(1, Seq(ModernSkill)),
+    FastTalk -> SkillInfo(5),
+    FirstAid -> SkillInfo(30),
+    History -> SkillInfo(5),
+    Hypnosis -> SkillInfo(1, Seq(UncommonSkill)),
+    Intimidate -> SkillInfo(15),
+    Jump -> SkillInfo(20),
+    Law -> SkillInfo(5),
+    LibraryUse -> SkillInfo(20),
+    Listen -> SkillInfo(20),
+    Locksmith -> SkillInfo(1),
+    MechanicalRepair -> SkillInfo(10),
+    Medicine -> SkillInfo(1),
+    NaturalWorld -> SkillInfo(10),
+    Navigate -> SkillInfo(10),
+    Occult -> SkillInfo(5),
+    OperateHeavyMachinery -> SkillInfo(1),
+    Persuade -> SkillInfo(10),
+    Psychoanalysis -> SkillInfo(1),
+    Psychology -> SkillInfo(10),
+    ReadLips -> SkillInfo(1, Seq(UncommonSkill)),
+    Ride -> SkillInfo(5),
+    SleightOfHand -> SkillInfo(10),
+    SpotHidden -> SkillInfo(25),
+    Stealth -> SkillInfo(20),
+    Swim -> SkillInfo(20),
+    Track -> SkillInfo(10),
     // Art and Craft
-    Acting -> 5,
-    FineArt -> 5,
-    Forgery -> 5,
-    Photography -> 5,
+    Acting -> SkillInfo(5),
+    FineArt -> SkillInfo(5),
+    Forgery -> SkillInfo(5),
+    Photography -> SkillInfo(5),
     // Lore
-    DreamLore -> 1,
-    NecronomiconLore -> 1,
-    UFOLore -> 1,
-    VampireLore -> 1,
-    WerewolfLore -> 1,
-    YaddithianLore -> 1,
+    DreamLore -> SkillInfo(1, Seq(UncommonSkill)),
+    NecronomiconLore -> SkillInfo(1, Seq(UncommonSkill)),
+    UFOLore -> SkillInfo(1, Seq(UncommonSkill)),
+    VampireLore -> SkillInfo(1, Seq(UncommonSkill)),
+    WerewolfLore -> SkillInfo(1, Seq(UncommonSkill)),
+    YaddithianLore -> SkillInfo(1, Seq(UncommonSkill)),
     // Science
-    Astronomy -> 1,
-    Biology -> 1,
-    Botany -> 1,
-    Chemistry -> 1,
-    Cryptography -> 1,
-    Engineering -> 1,
-    Forensics -> 1,
-    Geology -> 1,
-    Mathematics -> 1,
-    Meteorology -> 1,
-    Pharmacy -> 1,
-    Physics -> 1,
-    Zoology -> 1,
+    Astronomy -> SkillInfo(1),
+    Biology -> SkillInfo(1),
+    Botany -> SkillInfo(1),
+    Chemistry -> SkillInfo(1),
+    Cryptography -> SkillInfo(1),
+    Engineering -> SkillInfo(1),
+    Forensics -> SkillInfo(1),
+    Geology -> SkillInfo(1),
+    Mathematics -> SkillInfo(1),
+    Meteorology -> SkillInfo(1),
+    Pharmacy -> SkillInfo(1),
+    Physics -> SkillInfo(1),
+    Zoology -> SkillInfo(1),
     // Survival
-    Arctic -> 10,
-    Desert -> 10,
-    Sea -> 10
+    Arctic -> SkillInfo(10),
+    Desert -> SkillInfo(10),
+    Sea -> SkillInfo(10)
   )
 
-  val combatSkills: Map[CombatSkillName, Int] = Map(
-    Throw -> 20,
-    Axe -> 15,
-    Brawl -> 25,
-    Chainsaw -> 10,
-    Flail -> 10,
-    Garrote -> 15,
-    Spear -> 20,
-    Sword -> 20,
-    Whip -> 5,
-    Bow -> 15,
-    Handgun -> 20,
-    HeavyWeapons -> 10,
-    Flamethrower -> 10,
-    MachineGun -> 10,
-    RifleAndShotgun -> 25,
-    SubmachineGun -> 15
+  val combatSkills: Map[CombatSkillName, SkillInfo] = Map(
+    Throw -> SkillInfo(20),
+    Axe -> SkillInfo(15),
+    Brawl -> SkillInfo(25),
+    Chainsaw -> SkillInfo(10),
+    Flail -> SkillInfo(10),
+    Garrote -> SkillInfo(15),
+    Spear -> SkillInfo(20),
+    Sword -> SkillInfo(20),
+    Whip -> SkillInfo(5),
+    Bow -> SkillInfo(15),
+    Handgun -> SkillInfo(20),
+    HeavyWeapons -> SkillInfo(10),
+    Flamethrower -> SkillInfo(10),
+    MachineGun -> SkillInfo(10),
+    RifleAndShotgun -> SkillInfo(25),
+    SubmachineGun -> SkillInfo(15)
   )
 
   def basicSkill[A <: SimpleSkillName](
@@ -101,9 +101,9 @@ object SkillFactory {
       occupationPoints: Int,
       personalPoints: Int
   ): BasicSkill = {
-    val baseValue = basicSkills(name)
+    val SkillInfo(baseValue, tags) = basicSkills(name)
 
-    new BasicSkill(name, baseValue, occupationPoints, personalPoints)
+    new BasicSkill(name, baseValue, occupationPoints, personalPoints, tags)
   }
 
   def combatSkill[A <: CombatSkillName](
@@ -111,8 +111,8 @@ object SkillFactory {
       occupationPoints: Int,
       personalPoints: Int
   ): CombatSkill = {
-    val baseValue = combatSkills(name)
+    val SkillInfo(baseValue, tags) = combatSkills(name)
 
-    new CombatSkill(name, baseValue, occupationPoints, personalPoints)
+    new CombatSkill(name, baseValue, occupationPoints, personalPoints, tags)
   }
 }
