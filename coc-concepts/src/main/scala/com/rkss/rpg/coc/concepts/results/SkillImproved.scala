64d64975e5acf2a3bdef082964589e31de0dc282
malkaviano <rafael.kss@gmail.com>
2022-05-11 13:42:50 -0300

    fix: better generics

--- coc-concepts/src/main/scala/com/rkss/rpg/coc/concepts/results/SkillImproved.scala
+++ coc-concepts/src/main/scala/com/rkss/rpg/coc/concepts/results/SkillImproved.scala
@@ -2,7 +2,7 @@ package com.rkss.rpg.coc.concepts.results
 
 import com.rkss.rpg.coc.concepts.skill._
 
-final case class SkillImproved[A <: SkillName](
+final case class SkillImproved[A <: ImprovableSkillName](
     val name: A,
     val value: Int,
     val improvement: Int,
