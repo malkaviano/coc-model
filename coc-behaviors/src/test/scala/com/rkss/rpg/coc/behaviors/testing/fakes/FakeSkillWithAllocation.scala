84f5494b9857bce9b6dd0819fe134af3696133f4
malkaviano <rafael.kss@gmail.com>
2022-05-15 00:38:53 -0300

    refactor: remove allocation trait

--- coc-behaviors/src/test/scala/com/rkss/rpg/coc/behaviors/testing/fakes/FakeSkillWithAllocation.scala
+++ coc-behaviors/src/test/scala/com/rkss/rpg/coc/behaviors/testing/fakes/FakeSkillWithAllocation.scala
@@ -1,6 +1,5 @@
 package com.rkss.rpg.coc.behaviors.testing.fakes
 
-import com.rkss.rpg.coc.concepts.skill.allocation._
 import com.rkss.rpg.coc.concepts.skill._
 import com.rkss.rpg.coc.concepts.roll._
 import com.rkss.rpg.coc.behaviors.extractor._
@@ -8,11 +7,10 @@ import com.rkss.rpg.coc.behaviors.extractor._
 class FakeSkillWithAllocation[A <: AllocationSkillName](
     override val name: A,
     override val baseValue: Int,
-    override val occupationPoints: Int = 0,
-    override val personalPoints: Int = 0,
+    val occupationPoints: Int = 0,
+    val personalPoints: Int = 0,
     override val tags: Seq[SkillTag] = Seq.empty[SkillTag]
-) extends FakeSkill(name, baseValue, tags)
-    with SkillWithPointsAllocation {
+) extends FakeSkill(name, baseValue, tags) {
   override def value(difficulty: SkillRollDifficultyLevel): Int =
     DifficultyValueExtractor.value(
       baseValue + occupationPoints + personalPoints,
